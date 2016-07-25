f9ae587a30aa4412b6efa6b988139fb093b1dc79
Vitaly Takmazov <vitalyster@gmail.com>
2016-07-25 15:36:48 +0300

    using webpack as build system

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var webpack = require("webpack")
+var globby = require("globby")
+var ExtractTextPlugin = require("extract-text-webpack-plugin")
+module.exports = {
+    entry: {
+        "terminal" : globby.sync([
+            __dirname + "/lib/*.*"
+        ])
+    },
+    output: {
+        path: __dirname + "/dist",
+        filename: "[name].js"
+    },
+    module: {
+        loaders: [
+            { test: /\.less$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader") },
+            { test: /\.png$/, loader: "file" }
+	]
+    },
+    plugins: [
+        new ExtractTextPlugin("terminal.css", {
+            allChunks: true
+        })
+    ]
+}
